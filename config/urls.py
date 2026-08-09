167ba1e860098915e4d5dbe65d74bf59e24ea3f5
AbdulhamidovAsadbek <levuz5443@gmail.com>
2026-08-09 22:13:51 +0500

    Redeploy

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("Uzum.urls")),
     path("auth/", include("authentication.urls")),
-    path('webhook/', main.webhook, name='webhook'), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
