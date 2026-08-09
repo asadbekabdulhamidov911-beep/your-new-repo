d8505307fd7b08c306a5c89a45b0cfeb0518801c
AbdulhamidovAsadbek <levuz5443@gmail.com>
2026-08-09 22:11:46 +0500

    Redeploy

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
-from pdb import main
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import main
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("Uzum.urls")),
